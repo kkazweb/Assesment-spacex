8ad2e0270a0b55f36300786917143e575fa403bb
kkazweb <kazmierczakkacper18@gmail.com>
2024-06-16 18:00:20 +0200

    Initial commit
    Assessment spacex initial commit

---
+++ src/test/java/com/kkazmierczak/assessment/SpacexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkazmierczak.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
