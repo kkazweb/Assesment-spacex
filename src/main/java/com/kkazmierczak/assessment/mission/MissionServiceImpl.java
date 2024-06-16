b472310a6d1d5a95b91f43d5d970fe06cc2249a3
kkazweb <kazmierczakkacper18@gmail.com>
2024-06-16 19:58:48 +0200

    Add MissionServiceImpl methods body

--- src/main/java/com/kkazmierczak/assessment/mission/MissionServiceImpl.java
+++ src/main/java/com/kkazmierczak/assessment/mission/MissionServiceImpl.java
@@ -2,19 +2,27 @@ package com.kkazmierczak.assessment.mission;
 
 import com.kkazmierczak.assessment.rocket.Rocket;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class MissionServiceImpl implements MissionService {
     @Override
     public Mission createNew(long missionId, String name) {
-        return null;
+        return new Mission(missionId, name, Set.of());
     }
 
     @Override
     public Mission assignRocket(Mission mission, Rocket rocket) {
-        return null;
+        var rockets = Stream.concat(
+                        mission.getRockets().stream(),
+                        Stream.of(rocket))
+                .collect(Collectors.toSet());
+        return new Mission(mission.getMissionId(), mission.getName(), mission.getMissionStatus(), rockets);
     }
 
     @Override
     public Mission changeStatus(Mission mission, MissionStatus missionStatus) {
-        return null;
+        return new Mission(mission.getMissionId(), mission.getName(), missionStatus, mission.getRockets());
     }
 }
